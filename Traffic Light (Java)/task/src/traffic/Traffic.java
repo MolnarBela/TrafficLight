f16d1f0adc1774cde10b13992d32eb72b3275120
BMolnar_4D <bela_molnar@4dsoft.hu>
2023-10-28 06:08:29 +0200

    Stage3

---
+++ Traffic Light (Java)/task/src/traffic/Traffic.java
@@ -0,0 +1,74 @@
+package traffic;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Traffic {
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the traffic management system!");
+        System.out.print("Input the number of roads: ");
+        int roads = getValidNumber(scanner);
+        System.out.print("Input the interval: ");
+        int interval = getValidNumber(scanner);
+        int menu;
+        do {
+            clearTerminal();
+            System.out.println("Menu:\n" +
+                    "1. Add\n" +
+                    "2. Delete\n" +
+                    "3. System\n" +
+                    "0. Quit");
+            menu = scanner.nextInt();
+            printSelection(menu);
+            scanner.nextLine();
+        } while (menu != 0);
+        System.out.println("Bye!");
+    }
+    private void printSelection(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("Road added");
+                break;
+            case 2:
+                System.out.println("Road deleted");
+                break;
+            case 3:
+                System.out.println("System opened");
+                break;
+        }
+    }
+
+    private int getValidNumber(Scanner scanner) {
+        int number = 0;
+        boolean notValide = false;
+
+        do {
+            String roadsString = scanner.nextLine();
+            try {
+                number = Integer.parseInt(roadsString);
+            } catch (NumberFormatException e) {
+                System.out.print("Error! Incorrect Input. Try again: ");
+                continue;
+            }
+            if (number < 1) {
+                System.out.print("Error! Incorrect Input. Try again: ");
+            } else {
+                notValide = true;
+            }
+        } while (notValide);
+        return number;
+    }
+
+    private void clearTerminal() {
+        try {
+            var clearCommand = System.getProperty("os.name").contains("Windows")
+                    ? new ProcessBuilder("cmd", "/c", "cls")
+                    : new ProcessBuilder("clear");
+            clearCommand.inheritIO().start().waitFor();
+        }
+        catch (IOException | InterruptedException e) {}
+    }
+
+}
